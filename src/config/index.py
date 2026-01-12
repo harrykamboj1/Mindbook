4ee3ecd4267394e6774240c9d2103699a55ce724
harrykamboj1 <singhharnoor116@gmail.com>
2026-01-12 21:49:09 +0530

    Cloudlfare R2 setup & Celery setup & supabase Setup

---
+++ src/config/index.py
@@ -0,0 +1,61 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+if not os.getenv("SUPABASE_API_URL") or not os.getenv("SUPABASE_SECRET_KEY"):
+    raise ValueError(
+        "SUPABASE_API_URL and SUPABASE_SECRET_KEY must be set in .env file"
+    )
+
+if not os.getenv("CLERK_SECRET_KEY") or not os.getenv("DOMAIN"):
+    raise ValueError("CLERK_SECRET_KEY and DOMAIN must be set in .env file")
+
+
+# if (
+#     not os.getenv("S3_BUCKET_NAME")
+#     or not os.getenv("AWS_REGION")
+#     or not os.getenv("AWS_SECRET_ACCESS_KEY")
+#     or not os.getenv("AWS_ACCESS_KEY_ID")
+# ):
+#     raise ValueError(
+#         "S3_BUCKET_NAME, AWS_REGION, AWS_ACCESS_KEY_ID, and AWS_SECRET_ACCESS_KEY must be set in .env file"
+#     )
+
+if (
+    not os.getenv("R2_ACCESS_KEY")
+    or not os.getenv("R2_SECRET_KEY")
+    or not os.getenv("R2_BUCKET")
+    or not os.getenv("R2_ACCOUNT_ID")
+):
+    raise ValueError(
+        "R2_ACCESS_KEY, R2_SECRET_KEY, R2_BUCKET, and R2_ACCOUNT_ID must be set in .env file"
+    )
+
+if not os.getenv("REDIS_URL"):
+    raise ValueError("REDIS_URL must be set in .env file")
+
+if not os.getenv("OPENAI_API_KEY"):
+    raise ValueError("OPENAI_API_KEY must be set in .env file")
+
+if not os.getenv("SCRAPINGBEE_API_KEY"):
+    raise ValueError("SCRAPINGBEE_API_KEY must be set in .env file")
+
+if not os.getenv("TAVILY_API_KEY"):
+    raise ValueError("TAVILY_API_KEY must be set in .env file")
+
+
+appConfig = {
+    "supabase_api_url": os.getenv("SUPABASE_API_URL"),
+    "supabase_secret_key": os.getenv("SUPABASE_SECRET_KEY"),
+    "clerk_secret_key": os.getenv("CLERK_SECRET_KEY"),
+    "domain": os.getenv("DOMAIN"),
+    "r2_access_key": os.getenv("R2_ACCESS_KEY"),
+    "r2_secret_key": os.getenv("R2_SECRET_KEY"),
+    "r2_bucket": os.getenv("R2_BUCKET"),
+    "r2_account_id": os.getenv("R2_ACCOUNT_ID"),
+    "redis_url": os.getenv("REDIS_URL"),
+    "openai_api_key": os.getenv("OPENAI_API_KEY"),
+    "scrapingbee_api_key": os.getenv("SCRAPINGBEE_API_KEY"),
+    "tavily_api_key": os.getenv("TAVILY_API_KEY"),
+}
---
+++ src/services/cloudflareR2.py
@@ -0,0 +1,22 @@
+import boto3
+from botocore.client import Config
+from src.config import appConfig
+
+r2 = boto3.client(
+    "s3",
+    endpoint_url=f"https://{appConfig["r2_account_id"]}.r2.cloudflarestorage.com",
+    aws_access_key_id=appConfig["r2_access_key"],
+    aws_secret_access_key=appConfig["r2_secret_key"],
+    config=Config(signature_version="s3v4"),
+    region_name="auto",
+)
+
+
+def upload_file_to_r2(file_obj, filename: str, content_type: str):
+    r2.upload_fileobj(
+        file_obj,
+        appConfig["r2_bucket"],
+        filename,
+        ExtraArgs={"ContentType": content_type},
+    )
+    return f"https://{appConfig["r2_account_id"]}.r2.cloudflarestorage.com/{appConfig["r2_bucket"]}/{filename}"
